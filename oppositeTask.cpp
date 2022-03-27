fe10331a5e4336d9c988a485a7bd6e7a52d42877
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-27 11:34:08 +0600

    Opposite Task

---
+++ oppositeTask.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+    {
+        cin >> n;
+        if(n <= 10)
+        {
+            cout << n << " " << 0 << endl;
+        }
+        else
+        {
+            cout << 10 << " " << n - 10 << endl;
+        }
+    }
+}
